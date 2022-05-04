60cbe23258f6a2a85e2c1496df957f3bfe5a54d8
jackjiachenli <98990351+jackjiachenli@users.noreply.github.com>
2022-05-05 09:55:46 +1200

    final draft
    The program should be functioning as expected, now all there is to do is to finish databases and test both quizzes fully. Used functions to shorten the code. 
    Next steps: add more functionality eg. high score, do another quiz or leave, leaderboards, add comments, meet the instructions criteria

--- level-1-digital-technology.py
+++ level-1-digital-technology.py
@@ -2,10 +2,72 @@
 
 import sqlite3
 
+valid_answers = ["1", "2", "3", "4"]
+correct_answers = []
+
 DATABASE = "quiz.db"
 
 connection = sqlite3.connect(DATABASE)
 cursor = connection.cursor()
+
+
+
+def ask_continue():
+    ask_resume = True
+    while ask_resume == True:
+        try:
+            resume = int(input())
+            if resume == 1:
+                ask_resume = False
+            elif resume == 2:
+                ask_resume = False
+                ask_quiz_number = True
+            else:
+                raise ValueError
+        except ValueError:
+            print("Please enter 1 or 2")
+
+def print_quiz():
+    for result in results:
+        print(
+            f"""
+Question {result[0]}. {result[1]}
+Is it:
+1. {result[2]}
+2. {result[3]}
+3. {result[4]}
+4. {result[5]}
+"""
+        )
+        print("Please enter your answer as a number")
+        ask_answer = True
+        while ask_answer == True:
+            answer = input()
+            if answer == result[6]:
+                print("correct")
+                correct_answers.append(result[0])
+                ask_answer = False
+            elif answer != result[6] and answer in valid_answers:
+                print(
+                    f"""
+Incorrect
+The correct answer was {result[6]}"""
+                )
+                ask_answer = False
+            else:
+                print("Please enter a valid answer")
+
+
+def get_percentage():
+    length = cursor.fetchone()[0]
+    percentage = 100 * len(correct_answers) / length
+    print(
+        f"""
+You got {len(correct_answers)}/{length} correct
+That is {percentage}% correct!"""
+    )
+
+
 
 print(
     """
@@ -39,22 +101,10 @@ while ask_quiz_number == True:
 You have selected the Animals Quiz
 This quiz has 10 questions
 Are you sure you want to continue?
-Type 1 to continue or 2 to return
             """
             )
-            ask_resume = True
-            while ask_resume == True:
-                try:
-                    resume = int(input())
-                    if resume == 1:
-                        ask_resume = False
-                    elif resume == 2:
-                        ask_resume = False
-                        ask_quiz_number = True
-                    else:
-                        raise ValueError
-                except ValueError:
-                    print("Please enter 1 or 2")
+            print("Type 1 to continue or 2 to return")
+            ask_continue()
 
         elif int(quiz_number) == 2:
             ask_quiz_number = False
@@ -63,23 +113,22 @@ Type 1 to continue or 2 to return
 You have selected the Games Quiz
 This quiz has 10 questions
 Are you sure you want to continue?
-Type 1 to continue or 2 to return
             """
             )
-            ask_resume = True
-            while ask_resume == True:
-                try:
-                    resume = int(input())
-                    if resume == 1:
-                        ask_resume = False
-                    elif resume == 2:
-                        ask_resume = False
-                        ask_quiz_number = True
-                    else:
-                        raise ValueError
-                except ValueError:
-                    print("Please enter 1 or 2")
+            print("Type 1 to continue or 2 to return")
+            ask_continue()
         else:
             raise ValueError
     except ValueError:
         print("Please enter a valid quiz number")
+
+if quiz_number == 1:
+    results = cursor.execute("SELECT * FROM animals")
+    print_quiz()
+    cursor.execute("SELECT question_id FROM animals ORDER BY question_id DESC LIMIT 1")
+    get_percentage()
+elif quiz_number == 2:
+    results = cursor.execute("SELECT * FROM games")
+    print_quiz()
+    cursor.execute("SELECT question_id FROM games ORDER BY question_id DESC LIMIT 1")
+    get_percentage()
